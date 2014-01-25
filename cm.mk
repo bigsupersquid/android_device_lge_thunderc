22eb886ab75212b1570330b7e7152f1ca83c84b8
thekraven <savethestyle1@gmail.com>
2014-01-24 22:11:09 -0600

    update to vidcam fixes and latest build

--- cm.mk
+++ cm.mk
@@ -1,13 +1,11 @@
-# Include GSM stuff
-#$(call inherit-product, vendor/cm/config/cdma.mk)
 # Inherit device configuration for thunderc.
 $(call inherit-product, device/lge/thunderc/thunderc.mk)
-# Boot Animation
+
 TARGET_SCREEN_HEIGHT := 480
 TARGET_SCREEN_WIDTH := 320
-
 $(call inherit-product, vendor/cm/config/mini.mk)
 
+# Overrides
 PRODUCT_NAME := cm_thunderc
 PRODUCT_BRAND := lge
 PRODUCT_DEVICE := thunderc
@@ -18,6 +16,7 @@ PRODUCT_CHARACTERISTICS := phone
 # Release name and versioning
 PRODUCT_RELEASE_NAME := LS670
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=lge_LS670 \
     BUILD_FINGERPRINT="lge/LS670/LS670:2.3.3/GRI40/LG-LS670-V20g.19C11F164C:user/release-keys" \
