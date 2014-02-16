9821c5f2148b5860f6256e130cd5fc2395a79926
bigsupersquid <0.uradork.0@gmail.com>
2014-02-16 09:50:22 -0600

    shake dust off
    Change-Id: I0c2f665b9574e0b141951ec73769b3637b32fae2

--- cm.mk
+++ cm.mk
@@ -18,9 +18,9 @@ PRODUCT_RELEASE_NAME := VM670
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=lge_VM670 \
-    BUILD_FINGERPRINT="lge/LS670/LS670:2.3.3/GRI40/LG-LS670-V20g.19C11F164C:user/release-keys" \
-    PRIVATE_BUILD_DESC="thunderc-user 2.3.3 GRI40  LG-LS670-V20g.19C11F164C release-keys"
+    PRODUCT_NAME=lge_VM670 
+#    BUILD_FINGERPRINT="lge/LS670/LS670:2.3.3/GRI40/LG-LS670-V20g.19C11F164C:user/release-keys" \
+#    PRIVATE_BUILD_DESC="thunderc-user 2.3.3 GRI40  LG-LS670-V20g.19C11F164C release-keys"
 
 #override cm apns-conf
 PRODUCT_COPY_FILES += device/lge/thunderc/prebuilt/common/etc/apns-conf.xml:/system/etc/apns-conf.xml
