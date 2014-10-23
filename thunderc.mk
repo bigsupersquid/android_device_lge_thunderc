f380397461bb470fe005fcd6268f867f54c43dcf
bigsupersquid <0.uradork.0@gmail.com>
2014-10-23 17:08:59 -0500

    tweak for centered lockscreen text
    Change-Id: I75badd6bc8ecefff301aa1b8586150392fa13daa

--- thunderc.mk
+++ thunderc.mk
@@ -1,3 +1,5 @@
+PRODUCT_BOOT_JARS += qcmediaplayer
+
 # thunderc specific configs
 PRODUCT_COPY_FILES += \
    	$(LOCAL_PATH)/configs/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
@@ -15,20 +17,33 @@ PRODUCT_COPY_FILES += \
 #dex2oat \
 #core-libart \
 #libart
+
 PRODUCT_PACKAGES += \
-init.qcom.sh 
+init.qcom.sh \
+libdashplayer \
+qcmediaplayer
 #multirom \
 #trampoline \
 #multirom_zip \
 #multirom_uninstaller
 
-# Inherit products (Most specific first)
-$(call inherit-product, vendor/lge/thunderc/thunderc-vendor.mk)
-$(call inherit-product, device/lge/thunder-common/thunder-common.mk)
+#this is the USA not europe
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
+PRODUCT_LOCALES := en_US
 
 #recovery
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/fstab.thunderc:recovery/root/fstab.thunderc 
+    $(LOCAL_PATH)/fstab.thunderc:recovery/root/fstab.thunderc \
+    $(LOCAL_PATH)/recovery/twrp.fstab:recovery/root/etc/twrp.fstab
+
+#codecs
+#PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
+
+# Inherit products (Most specific first)
+$(call inherit-product, vendor/lge/thunderc/thunderc-vendor.mk)
+$(call inherit-product, device/lge/thunder-common/thunder-common.mk)
 
 # Overrides
 PRODUCT_NAME := thunderc
@@ -36,28 +51,14 @@ PRODUCT_DEVICE := thunderc
 PRODUCT_MODEL := LG-VM670
 PRODUCT_MANUFACTURER := LGE
 
-#this is the USA not europe
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf
-PRODUCT_LOCALES := en_US
-
 #sysctl tweaks
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/sysctl.conf:system/etc/sysctl.conf
 
-#PurePerformances tweaks
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/S70darky_zipalign:system/etc/init.d/S70darky_zipalign
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/S98system_tweak:system/etc/init.d/S98system_tweak
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/sqlite_optimize:system/etc/init.d/sqlite_optimize
-
 #fsck_f2fs
-#PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/01fsck_f2fs:system/etc/init.d/01fsck_f2fs
+PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/01fsck_f2fs:system/etc/init.d/01fsck_f2fs
 
 #minfree and oom controls
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/59minfree:system/etc/init.d/59minfree
-
-#codecs
-#PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
 
 CDMA_GOOGLE_BASE := android-sprint-us
 CDMA_CARRIER_ALPHA := Virgin Mobile
